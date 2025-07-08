942c22487b1b8de45b2cd954b3362d94adbec5bb
Bamgeglo1485 <akkauntmin@gmail.com>
2025-07-09 01:45:31 +0900

    WHAT THE FUCK IS ;

--- Content.Server/Vanilla/Objectives/Systems/EscapeLondonersConditionSystem.cs
+++ Content.Server/Vanilla/Objectives/Systems/EscapeLondonersConditionSystem.cs
@@ -6,7 +6,7 @@ using Robust.Shared.Player;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
 using Content.Server.Mind;
-using Content.Server.Objectives
+using Content.Server.Objectives;
 
 namespace Content.Server.Objectives.Systems;
 
