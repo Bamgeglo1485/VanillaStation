a3b54daf098bebd67d9f429bc472c2d95abb07b3
Bamgeglo1485 <akkauntmin@gmail.com>
2025-08-21 01:47:11 +0900

    Patch 1

---
+++ Content.Server/Vanilla/Teleportation/RandomPortalSystem.cs
@@ -0,0 +1,272 @@
+using Content.Server.Administration.Logs;
+using Content.Shared.Database;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Popups;
+using Content.Shared.Teleportation.Components;
+using Content.Shared.Teleportation.Systems;
+using Robust.Server.Audio;
+using Robust.Server.GameObjects;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Physics.Systems;
+using System.Linq;
+using System.Numerics;
+using Robust.Shared.GameObjects;
+using Content.Server.Station.Components;
+using Robust.Shared.Physics.Components;
+using Content.Server.Procedural;
+using Content.Shared.Gravity;
+using Content.Shared.Procedural;
+using Robust.Shared.Timing;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Parallax;
+using Content.Shared.Atmos;
+using Content.Shared.Parallax.Biomes;
+using Content.Shared.Atmos.Components;
+using Content.Shared.Parallax;
+using Content.Server.Atmos.Components;
+using Robust.Shared.Map;
+using Content.Shared.Atmos.Prototypes;
+using System.Collections.Generic;
+using Content.Server.Station.Systems;
+using Content.Server.Station.Components;
+
+namespace Content.Server.Teleportation;
+
+public sealed class RandomPortalSystem : EntitySystem
+{
+    [Dependency] private readonly LinkedEntitySystem _link = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedMapSystem _mapSystem = default!;
+    [Dependency] private readonly DungeonSystem _dungeonSystem = default!;
+    [Dependency] private readonly BiomeSystem _biomeSystem = default!;
+    [Dependency] private readonly AtmosphereSystem _atmosphereSystem = default!;
+    [Dependency] private readonly StationSystem _stationSystem = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RandomPortalComponent, ComponentInit>(OnComponentInit);
+    }
+
+    private void OnComponentInit(Entity<RandomPortalComponent> entity, ref ComponentInit args)
+    {
+        var transform = Transform(entity);
+        var mapId = transform.MapID;
+
+        if (entity.Comp.OnlyInStationTeleport)
+            StationTeleport(entity, transform, mapId);
+        else if (entity.Comp.OnlyInMapTeleport)
+            InMapTeleport(entity, transform, mapId);
+        else
+            DimensionalTeleport(entity, transform, mapId);
+    }
+
+    private void StationTeleport(Entity<RandomPortalComponent> portal, TransformComponent trans, MapId mapId)
+    {
+        var stationGrid = FindStationGrid();
+        if (stationGrid == null)
+        {
+            InMapTeleport(portal, trans, mapId);
+            return;
+        }
+
+        if (!TryComp<MapGridComponent>(stationGrid, out var grid))
+        {
+            InMapTeleport(portal, trans, mapId);
+            return;
+        }
+
+        if (!TryFindSuitableTile(stationGrid.Value, grid, out var coords))
+            return;
+
+        SpawnAndLinkPortal(portal, coords);
+    }
+
+    private EntityUid? FindStationGrid()
+    {
+        var query = EntityQueryEnumerator<StationDataComponent>();
+        while (query.MoveNext(out var stationUid, out var stationData))
+        {
+            var gridUid = _stationSystem.GetLargestGrid(stationData);
+            if (gridUid == null)
+                continue;
+
+            var gridTrans = Transform(gridUid.Value);
+            if (gridTrans.MapID != MapId.Nullspace)
+                return gridUid.Value;
+        }
+        return null;
+    }
+
+    private void InMapTeleport(Entity<RandomPortalComponent> portal, TransformComponent xform, MapId mapId)
+    {
+        var validGrids = EntityQuery<MapGridComponent, TransformComponent>()
+            .Where(x => x.Item2.MapID == mapId && !HasComp<BecomesStationComponent>(x.Item2.GridUid))
+            .Select(x => (x.Item2.GridUid, x.Item1))
+            .ToList();
+
+        if (validGrids.Count == 0)
+            return;
+
+        var (gridUid, grid) = _random.Pick(validGrids);
+        if (!TryFindSuitableTile(gridUid!.Value, grid, out var coords))
+            return;
+
+        SpawnAndLinkPortal(portal, coords);
+    }
+
+    private bool TryFindSuitableTile(EntityUid gridUid, MapGridComponent gridComp, out EntityCoordinates coords)
+    {
+        var validTiles = new List<Vector2i>();
+        var tileEnumerator = _mapSystem.GetAllTilesEnumerator(gridUid, gridComp, true);
+
+        while (tileEnumerator.MoveNext(out var tile))
+        {
+            var worldPos = _mapSystem.GridTileToWorld(gridUid, gridComp, tile.Value.GridIndices);
+            var box = new Box2(worldPos.Position - new Vector2(0.5f), worldPos.Position + new Vector2(0.5f));
+
+            if (!_lookup.GetEntitiesIntersecting(worldPos.MapId, box).Any(e => e != gridUid))
+                validTiles.Add(tile.Value.GridIndices);
+        }
+
+        if (validTiles.Count == 0)
+        {
+            coords = default;
+            return false;
+        }
+
+        coords = _mapSystem.GridTileToLocal(gridUid, gridComp, _random.Pick(validTiles));
+        return true;
+    }
+
+    private void SpawnAndLinkPortal(Entity<RandomPortalComponent> portal, EntityCoordinates coords)
+    {
+        var exitPortal = Spawn(portal.Comp.SecondPortalPrototype, coords);
+        _link.TryLink(portal, exitPortal, true);
+    }
+
+    private void DimensionalTeleport(Entity<RandomPortalComponent> portal, TransformComponent transform, MapId mapId)
+    {
+        if (_random.Prob(0.3f))
+            CreateDungeonMap(portal);
+        else
+            CreatePlanet(portal);
+    }
+
+    private async void CreateDungeonMap(Entity<RandomPortalComponent> portal)
+    {
+        if (portal.Comp.AllowedDungeons.Count == 0)
+            return;
+
+        var mapId = _mapManager.CreateMap();
+        var mapUid = _mapManager.GetMapEntityId(mapId);
+
+        var gravity = EnsureComp<GravityComponent>(mapUid);
+        gravity.Enabled = true;
+
+        EnsureComp<PortalMapComponent>(mapUid);
+
+        if (portal.Comp.AllowedParallaxes.Count > 0)
+        {
+            var parallax = EnsureComp<ParallaxComponent>(mapUid);
+            parallax.Parallax = _random.Pick(portal.Comp.AllowedParallaxes);
+        }
+
+        var gridUid = _mapManager.CreateGrid(mapId).Owner;
+        if (!TryComp<MapGridComponent>(gridUid, out var gridComp))
+        {
+            _mapManager.DeleteMap(mapId);
+            return;
+        }
+
+        var dungeonProto = _prototype.Index<DungeonConfigPrototype>(_random.Pick(portal.Comp.AllowedDungeons));
+        await _dungeonSystem.GenerateDungeonAsync(dungeonProto, gridUid, gridComp, Vector2i.Zero, _random.Next());
+
+        if (_mapManager.MapExists(mapId) && TryFindSuitableTile(gridUid, gridComp, out var coords))
+            SpawnAndLinkPortal(portal, coords);
+    }
+
+    private void CreatePlanet(Entity<RandomPortalComponent> portal)
+    {
+        if (portal.Comp.AllowedPlanets.Count == 0)
+            return;
+
+        var mapId = _mapManager.CreateMap();
+        _mapManager.SetMapPaused(mapId, false);
+        var mapUid = _mapManager.GetMapEntityId(mapId);
+
+        var biomeProto = _prototype.Index<BiomeTemplatePrototype>(_random.Pick(portal.Comp.AllowedPlanets));
+        _biomeSystem.EnsurePlanet(mapUid, biomeProto);
+
+        var grid = _mapManager.CreateGrid(mapId);
+        var gridUid = grid.Owner;
+
+        EnsureComp<PortalMapComponent>(mapUid);
+
+        if (!TryComp<MapGridComponent>(gridUid, out var gridComp))
+        {
+            _mapManager.DeleteMap(mapId);
+            return;
+        }
+
+        Gas[] primaryGases;
+        Gas[] secondaryGases;
+
+        if (_random.Prob(0.6f))
+        {
+            primaryGases = new Gas[]
+            {
+            Gas.Oxygen,
+            Gas.Nitrogen,
+            Gas.CarbonDioxide,
+            Gas.Plasma,
+            Gas.Tritium,
+            Gas.Ammonia,
+            Gas.NitrousOxide
+            };
+        }
+        else
+        {
+            primaryGases = new Gas[]
+            {
+            Gas.Oxygen,
+            Gas.WaterVapor,
+            Gas.Nitrogen
+            };
+        }
+
+        var mixture = new GasMixture(2500)
+        {
+            Temperature = _random.Prob(0.6f)
+                ? _random.NextFloat(72f, 400f)  
+                : _random.NextFloat(273f, 333f) 
+        };
+
+        var selectedGases = primaryGases
+            .OrderBy(_ => _random.Next())
+            .Take(2)
+            .ToList();
+
+        foreach (var gas in selectedGases)
+        {
+            mixture.AdjustMoles(gas, _random.NextFloat(40f, 150f));
+        }
+
+        if (primaryGases.Length > 2 && _random.Prob(0.3f))
+        {
+            var traceGas = _random.Pick(primaryGases.Except(selectedGases).ToArray());
+            mixture.AdjustMoles(traceGas, _random.NextFloat(1f, 5f));
+        }
+
+        _atmosphereSystem.SetMapAtmosphere(mapUid, false, mixture);
+
+        var zeroCoords = _mapSystem.GridTileToLocal(gridUid, gridComp, Vector2i.Zero);
+        var exitPortal = Spawn(portal.Comp.SecondPortalPrototype, zeroCoords);
+        _link.TryLink(portal, exitPortal, true);
+    }
+}
