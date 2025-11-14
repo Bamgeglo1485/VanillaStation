d9ef3255b62935c80fc994510b478e45b92b792c
Lemniscate <mail@rayten.com>
2025-11-14 20:54:40 +0300

    абскостюмчикфикс

---
+++ Content.Server/Vanilla/entities/Clothing/ClothingPressureImmunity/ClothingPressureImmunityComponent.cs
@@ -0,0 +1,6 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Server.Vanilla.Entities.ClothingPressureImmunity;
+///выдает имун к давлению при надевании
+[RegisterComponent]
+public sealed partial class ClothingPressureImmunityComponent : Component { }
